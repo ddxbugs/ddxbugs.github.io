04c67383c1a1c7e93332b39a4c5e0fc15fb420b5
ddxbugs <63527442+ddxbugs@users.noreply.github.com>
2022-10-15 10:48:52 -0700

    initial commit about.js, button functionality

---
+++ js/about.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    // display about section
+    document.getElementById("foreground").innerhtml=`
+        <section>
+            <div>
+                <h1>header</h1>
+            </div>
+            <div>
+                <p>paragraph 1</p>
+            </div>
+        </section>
+    `;
+        
+    
+}
